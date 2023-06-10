853e9b75fc5a5dc88d225735e2ded084b9976273
JYLN <14062951+JYLN@users.noreply.github.com>
2023-06-10 08:51:04 -0600

    Add more pieces of relevant state

--- codecademy-reddit-client/src/store/redditSlice.js
+++ codecademy-reddit-client/src/store/redditSlice.js
@@ -4,10 +4,17 @@ const redditSlice = createSlice({
   name: 'reddit',
   initialState: {
     selectedSubreddit: '',
+    previousSelectedSubreddit: [],
+    posts: [],
+    loadingPosts: false,
+    failedLoadingPosts: false,
   },
   reducers: {
     setSelectedSubreddit: (state, action) => {
       state.selectedSubreddit = action.payload;
+    },
+    addPreviousSelectedSubreddit: (state, action) => {
+      state.previousSelectedSubreddit = [action.payload, ...state.previousSelectedSubreddit];
     },
   },
 });
@@ -16,3 +23,4 @@ export const { setSelectedSubreddit } = redditSlice.actions;
 export default redditSlice.reducer;
 
 export const getSelectedSubreddit = state => state.reddit.selectedSubreddit;
+export const getPreviousSelectedSubreddit = state => state.reddit.previousSelectedSubreddit;
